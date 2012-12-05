095ad79ea3c6c5acafb43f5b965bbb2b8da66dae
Facundo Schwindt <facu0907@gmail.com>
2012-12-05 12:08:29 -0300

    test git commit 2

--- DMS Doppler Counter/ClockBViewController.h
+++ DMS Doppler Counter/ClockBViewController.h
@@ -10,6 +10,6 @@
 
 @interface ClockBViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UIPageControl *clockBPageController;
+@property (weak, nonatomic) IBOutlet UIPageControl *clockBPageController ;
 
 @end
